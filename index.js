40ad3d9b729c1e20a1f38263775d0f0ee39245bf
Aleksey <alekseyhonchar@gmail.com>
2021-09-29 17:04:46 +0300

    Fixed sonar

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function square(number) {
   return number * number;
 }
 console.log(square(2));
+
+const b = 10;
+if (b === 0) {
+  square(2);
+} else if (b === 1) {
+  square(3);
+} else {
+  console.log(123);
+}
